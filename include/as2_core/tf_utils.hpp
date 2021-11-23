b21306615a9d0ed2b08ffa549d72e228d03960d6
perezsaura-david <perezsaura.david@gmail.com>
2021-11-23 13:34:21 +0100

    Changes as2

---
+++ include/as2_core/tf_utils.hpp
@@ -0,0 +1,22 @@
+#ifndef TF_UTILS_HPP_
+#define TF_UTILS_HPP_
+
+#include <string>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+
+std::string generateTfName(std::string _namespace, std::string _frame_name);
+
+geometry_msgs::msg::TransformStamped getTransformation(	const std::string& _frame_id,
+												  const std::string& _child_frame_id,
+													double _translation_x,
+													double _translation_y,
+													double _translation_z,
+													double _roll,
+													double _pitch,
+													double _yaw  );
+
+#endif  //TF_UTILS_HPP_
