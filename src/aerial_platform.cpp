bbd4a143f7d3fc217aeed18ea86a3e1213efccc4
Pedro Arias <pedroariasperez96@gmail.com>
2022-06-02 11:57:39 +0200

    Default parameter values updated

--- src/aerial_platform.cpp
+++ src/aerial_platform.cpp
@@ -44,12 +44,52 @@ namespace as2
 
     this->declare_parameter<float>("cmd_freq", 100.0);
     this->declare_parameter<float>("info_freq", 10.0);
-    this->declare_parameter<bool>("simulation_mode", false);
-    this->declare_parameter<float>("mass", 1.0);
-    this->declare_parameter<float>("max_thrust", 0.0);
-    this->declare_parameter<float>("min_thrust", 0.0);
-    this->declare_parameter<std::string>("control_modes_file", "config/control_modes.yaml");  // FIXME: not found
-
+    try
+    {
+      this->declare_parameter<bool>("simulation_mode");
+    }
+    catch(const rclcpp::ParameterTypeException& e)
+    {
+      RCLCPP_FATAL(this->get_logger(), "Launch argument <simulation_mode> not defined or malformed: %s", e.what());
+      this->~AerialPlatform();
+    }
+    try
+    {
+      this->declare_parameter<float>("mass");
+    }
+    catch(const rclcpp::ParameterTypeException& e)
+    {
+      RCLCPP_FATAL(this->get_logger(), "Launch argument <mass> not defined or malformed: %s", e.what());
+      this->~AerialPlatform();
+    }
+    try
+    {
+      this->declare_parameter<float>("max_thrust");
+    }
+    catch(const rclcpp::ParameterTypeException& e)
+    {
+      RCLCPP_FATAL(this->get_logger(), "Launch argument <max_thrust> not defined or malformed: %s", e.what());
+      this->~AerialPlatform();
+    }
+    try
+    {
+      this->declare_parameter<float>("min_thrust");
+    }
+    catch(const rclcpp::ParameterTypeException& e)
+    {
+      RCLCPP_FATAL(this->get_logger(), "Launch argument <min_thrust> not defined or malformed: %s", e.what());
+      this->~AerialPlatform();
+    }
+    try
+    {
+      this->declare_parameter<std::string>("control_modes_file");
+    }
+    catch(const rclcpp::ParameterTypeException& e)
+    {
+      RCLCPP_FATAL(this->get_logger(), "Launch argument <control_modes_file> not defined or malformed: %s", e.what());
+      this->~AerialPlatform();
+    }
+    
     this->get_parameter("cmd_freq", cmd_freq_);
     this->get_parameter("info_freq", info_freq_);
     this->get_parameter("simulation_mode", parameters_.simulation_mode);
